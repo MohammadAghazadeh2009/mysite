880e5a1ffb773a969f13f2c1f4f6352d31695f6c
MohammadAghazadeh <mohammadaghazadeh256@gmail.com>
2025-03-13 12:12:00 +0330

    data base
    migrate a data base

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from website.views import *
-
+app_name = "website"
 urlpatterns = [
-    path("", home),
-    path("about", about),
-    path("contact", contact),
+    path("", home, name="home"),
+    path("about", about, name="about"),
+    path("contact", contact, name="contact"),
 ]
